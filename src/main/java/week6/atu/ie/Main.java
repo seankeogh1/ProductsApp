fa3c5e60095920a60cd93c3f4351b4226f04dffd
SEANKEOGH-STUDENT <G00432475@atu.ie>
2024-10-23 16:30:47 +0100

    Try Catch

---
+++ src/main/java/week6/atu/ie/Main.java
@@ -0,0 +1,9 @@
+package week6.atu.ie;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Please enter a number between 0 -9: ");
+        Scanner myscan = new Scanner(System.in);
+    }
+}
