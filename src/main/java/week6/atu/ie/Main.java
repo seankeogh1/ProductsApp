0a793770e0c31bf8721ef36b4120a7bda056a6bd
SEANKEOGH-STUDENT <G00432475@atu.ie>
2024-10-23 16:40:03 +0100

    Try Catch step 1

--- src/main/java/week6/atu/ie/Main.java
+++ src/main/java/week6/atu/ie/Main.java
@@ -5,5 +5,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Please enter a number between 0 -9: ");
         Scanner myscan = new Scanner(System.in);
+
+        String input = myscan.nextLine();
+
+        int myNum = Integer.parseInt(input);
+
+        if(myNum >= 0 && myNum <= 9){
+            System.out.println("You entered " +myNum);
+        }
+        else{
+            System.out.println("Not a valid number: ");
+        }
     }
 }
